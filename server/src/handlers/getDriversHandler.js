ba6139b1428cb84f7f14529e388310db26d1e702
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-02-15 17:42:49 -0300

    getDriversHandlers Comments Updated

--- server/src/handlers/getDriversHandler.js
+++ server/src/handlers/getDriversHandler.js
@@ -19,9 +19,19 @@ module.exports = {
     getDetailHandler
 };
 
-//TODO: Handler -> Recibe la req -> Unifica datos -> Devuelve res -> Invoca a controller (que es otra fx) | NUNCA INTERACTÚA CON FUENTES EXTERNAS
-/* //TODO: Para solicitar información:
-    /:id -> params
-    query -> /drivers?forname=FeFi&surname=Lisperguer
-    body -> info
+/*
+TODO: CONTROLLER:Es el que hace la interacción con fuentes externas de información
+-> API
+-> DB
+
+TODO: HANDLER: Manejo interno de información (recibida ya sea por query, params, body) - NUNCA INTERACTÚA CON FUENTES EXTERNAS
+-> Recibe la req
+-> Unifica datos
+-> Devuelve res
+-> Invoca a controller (que es otra fx)
+
+TODO: SOLICITUD DE INFORMACIÓN:
+-> /:id -> params
+-> query -> /drivers?forname=FeFi&surname=Lisperguer
+-> body -> info
 */
