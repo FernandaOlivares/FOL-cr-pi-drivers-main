78a9edc853c7b6b89161d04bf0020d7f3da93a4f
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-02-15 17:26:51 -0300

    createDriverHandler by body Updated!

--- server/src/handlers/createDriverHandler.js
+++ server/src/handlers/createDriverHandler.js
@@ -1,7 +1,16 @@
 const createDriverHandler = (req, res) =>{
-    res.status(201).send('Crear driver');
+    const { forname, surname, nationality } = req.body;
+    res.status(201).send(`Driver ${forname} ${surname}, ${nationality}, successfully created. Well done!`);
 };
 //TODO: POST o PUT: 201 -> "created"
+
+/*
+{
+  "forname" : "Fernanda",
+  "surname" : "Lisperguer",
+  "nationality" : "chilienne"
+}
+*/
 
 module.exports = {
     createDriverHandler
