ab3ce2611b418f0d53511852eb6d05e2254e7d86
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-02-15 14:03:56 -0300

    Routes Modularization

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,4 +2,19 @@ const { Router } = require("express");
 
 const router = Router();
 
+//TODO: CONFIGURAMOS NUESTRO ROUTER PRINCIPAL, DONDE LLEGARÁN TODOS LOS ENDPOINTS
+
+router.get('/drivers', (req, res) =>{
+    res.status(200).send("Aquí están todos los drivers!=)");
+    });
+
+router.get('/drivers/:id', (req, res) =>{
+    res.status(200).send('Aquí está el detalle del driver!=)');
+    });
+
+router.post('/drivers', (req, res) =>{
+    res.status(200).send('Crear driver');
+    });
+
+
 module.exports = router;
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = require("./routes");
+const router = require("./routes/index");
 const morgan = require("morgan");
 const cors = require("cors");
 
