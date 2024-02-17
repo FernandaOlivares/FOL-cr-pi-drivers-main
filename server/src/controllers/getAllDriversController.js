cecbe1481b866ed86936efe8b4ecefdf8005dfaf
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-02-17 18:40:38 -0300

    Drivers From A to Z

--- server/src/controllers/getAllDriversController.js
+++ server/src/controllers/getAllDriversController.js
@@ -2,12 +2,24 @@ const getApiInfo = require('../helpers/getApiInfo');
 const getDbInfo = require('../helpers/getDbInfo');
 
 
+
 const getAllDrivers = async () => {
+    const apiInfo = await getApiInfo();
+    const dbInfo = await getDbInfo();
+    const allDrivers = [...apiInfo, ...dbInfo];
+
+    allDrivers.sort((a, b) => a.forename.localeCompare(b.forename));
+
+    return allDrivers;
+};
+/*
+const getAllDriver = async () => {
     const apiInfo = await getApiInfo();
     const dbIndo = await getDbInfo();
     const allDrivers = [...apiInfo, ...dbIndo];
     return allDrivers;
 };
+*/
 
 
 module.exports = getAllDrivers;
