b6967defeb0254d373ea369102edd4fd12578fbf
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-02-17 17:53:25 -0300

    Return First 15th Drivers

--- server/src/controllers/getDriverByNameController.js
+++ server/src/controllers/getDriverByNameController.js
@@ -4,7 +4,7 @@ const getAllDrivers = require ('../controllers/getAllDriversController');
 const getDriverByName = async(forename) => {
     const allDrivers = await getAllDrivers(forename);
     const driversFilteredByName = allDrivers.filter(driver => driver.forename.toLowerCase().includes(forename.toLowerCase()));
-    return driversFilteredByName;
+    return driversFilteredByName.slice(0, 15);
 };
 
 
