037b8505e1ad876dc9d42b62bc408916917ae831
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-02-17 17:42:12 -0300

    Props Info Order Updated

--- server/src/helpers/getApiInfo.js
+++ server/src/helpers/getApiInfo.js
@@ -6,19 +6,18 @@ const getApiInfo = async () =>{
     const apiUrl = await axios.get('http://localhost:5000/drivers/');
     const apiInfo = await apiUrl.data.map((driver) => {
         
-        return {
-            id: driver.id,
-            forename: driver.name.forename,
-            surname: driver.name.surname,
-            description: driver.description,
-            image: driver.image?.url ?? imgDefault,
-            nationality: driver.nationality,
-            dateOfBirth: driver.dob,
-            teams: (driver.teams && driver.teams.split(',').map(team => team.trim())) ?? ['Up!No teams were found...'],
-            created: false,
-        }
-    });
-    return apiInfo;
+    return {
+        id: driver.id,
+        forename: driver.name.forename,
+        surname: driver.name.surname,
+        nationality: driver.nationality,
+        dateOfBirth: driver.dob,
+        teams: (driver.teams && driver.teams.split(',').map(team => team.trim())) ?? ['Up!No teams were found...'],
+        image: driver.image?.url ?? imgDefault,
+        description: driver.description,
+        created: false,
+    }});
+return apiInfo;
 };
 
 
