4fc68d90037c369ce4fc4115ba4e1507129e2bca
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-02-18 15:59:24 -0300

    Code Organization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
+    <h1>Hello World =D!</h1>
         <Home/>
         <Detail/>
         <Create/>
--- client/src/views/LandingPage/LandingPage.jsx
+++ client/src/views/LandingPage/LandingPage.jsx
@@ -1,15 +1,10 @@
-//import React from "react";
-import { Link } from 'react-router-dom';
-import BackgroundVideo from "../../components/BackgroundVideo/BackgroundVideo";
-import style from "./LandingPage.module.css";
+//import { Link } from 'react-router-dom';
+//import BackgroundVideo from "../../components/BackgroundVideo/BackgroundVideo";
+//import style from "./LandingPage.module.css";
 
 const LandingPage = () => {
   return (
-    <div className= {style.container}>
-       <Link to="/home">
-      <button className={style.overlayButton}> S T A R T </button>
-      <BackgroundVideo />
-      </Link>
+    <div>
     </div>
   );
 };
