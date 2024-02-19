2996bd0efb2e9eacc0a65e19ec7a34bb4a455da5
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-02-19 20:50:21 -0300

    Extra validations! =)

--- server/src/models/TeamsModel.js
+++ server/src/models/TeamsModel.js
@@ -1,20 +1,20 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una función que define el modelo
-// Luego le inyectamos la conexión a sequelize.
-module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('Team', {
 
+module.exports = (sequelize) => {
+  sequelize.define('Team', {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
       unique: true,
-      allowNull: false
+      allowNull: false,
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        len: [2, 30],
+      }
     }
   },
   { timestamps: false }
