3d80cb19c437e603395be72e5379743d4fecd3e2
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-02-20 17:28:47 -0300

    Img Undefine Message Updated

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
 import styles from './Card.module.css';
 import defaultImg from '../../assets/f1HeroDefaultImg.jpg'
 
@@ -10,7 +9,8 @@ const Card = ({ forename, surname, image }) => {
       <h3>{forename}</h3>
       <p>{surname}</p>
       <img className={styles.imgContainer}
-      src={image ? image : defaultImg}/>
+      src={image ? image : defaultImg}
+      alt={`${forename} ${surname}'s picture not found`}/>
     </div>
   );
 };
