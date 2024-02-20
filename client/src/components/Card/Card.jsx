7c99c1f56f0279e399d8bc72322141d64a9d6ff8
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-02-20 17:42:26 -0300

    Styles Updating

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -6,8 +6,8 @@ import defaultImg from '../../assets/f1HeroDefaultImg.jpg'
 const Card = ({ forename, surname, image }) => {
   return (
     <div className={styles.card}>
-      <h3>{forename}</h3>
-      <p>{surname}</p>
+      <h3 className={styles.overlayTextName}>{forename}</h3>
+      <p className={styles.overlayTextName}>{surname}</p>
       <img className={styles.imgContainer}
       src={image ? image : defaultImg}
       alt={`${forename} ${surname}'s picture not found`}/>
