7830c43808f200b86ad09bff8195f04acaf27939
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-02-22 18:06:50 -0300

    Link to Detail Updated

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,23 +1,24 @@
 /* eslint-disable react/prop-types */
 import styles from './Card.module.css';
-import defaultImg from '../../assets/f1HeroDefaultImg.jpg'
-
+import defaultImg from '../../assets/f1HeroDefaultImg.jpg';
+import { Link } from 'react-router-dom';
 
 const Card = ({eachDriver}) => {
-  const { forename, surname, image, teams } = eachDriver;
+  const { forename, surname, image, teams, id } = eachDriver;
   const joinTeams = (teams && teams.length > 0) ? teams.join(" - ") : (teams ?? "Independent Driver");
 
 
 
   return (
-    <div className={styles.card}>
+    <div className={styles.card}> 
+      <Link to={`/home/${id}`}>
       <img className={`${styles.imageFilter}`}
       src={image ? image : defaultImg}
       alt={`${forename} ${surname}'s picture not found`}/>
       <h2 className={`${styles.overlayTextName} ${styles.robotoMonoFont}`}>{forename}</h2>
       <h2 className={`${styles.overlayTextName} ${styles.robotoMonoFontBolt }`}>{surname}</h2>
       <p className={`${styles.overlayTextName} ${styles.robotoMonoFont}`}>{joinTeams}</p>
-      
+      </Link>
     </div>
   );
 };
