995eeb68d83daa0c0bf0521b5d485c46f986505e
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-02-29 20:20:16 -0300

    teams prop updated to strings array

--- server/src/helpers/getDbInfo.js
+++ server/src/helpers/getDbInfo.js
@@ -1,5 +1,43 @@
 const { Driver, Team } = require('../db');
 
+const getDbInfo = async () => {
+    // Obtener los datos de la base de datos
+    const driversDb = await Driver.findAll({
+        include: {
+            model: Team,
+            attributes: ['name'], // Solo obtenemos el nombre del equipo
+            through: { attributes: [] }
+        }
+    });
+
+    // Mapeamos cada conductor para incluir también la información de los equipos
+    const driversWithTeams = driversDb.map(driver => {
+        // Mapeamos los nombres de los equipos
+        const teams = driver.Teams.map(team => team.name);
+        console.log(teams);
+        
+        // Retornamos un nuevo objeto que contiene toda la información del conductor
+        return {
+            id: driver.id,
+            forename: driver.forename,
+            surname: driver.surname,
+            nationality: driver.nationality,
+            dateOfBirth: driver.dateOfBirth,
+            teams: teams, // Agregamos los nombres de los equipos al objeto del conductor
+            image: driver.image,
+            description: driver.description,
+            created: driver.created,
+        };
+    });
+
+    return driversWithTeams;
+};
+
+module.exports = getDbInfo;
+
+
+/*const { Driver, Team } = require('../db');
+
 
 const getDbInfo = async () => {
     const driversDb = await Driver.findAll({
@@ -13,4 +51,4 @@ const getDbInfo = async () => {
 };
 
 
-module.exports = getDbInfo;
+module.exports = getDbInfo;*/
