f6b97e1ddeb3897cc5839c09fe807f712df137d9
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-03-04 09:11:10 -0300

    Header Styles OK

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = ({pageTitle}) => {
     return (
         <div>
-            <h1 className={styles.logoContainer}>F1 - LEGENDS</h1>
+            <Link to="/home" className={styles.logoContainer}>F1 - LEGENDS</Link>
             <h2 className={styles.titleContainer}>{pageTitle}</h2>
+            <div className={styles.buttonContainer}>
+                <Link className={styles.linkButton}  to='/home'>Home</Link>
+            </div>
         </div>
     );
 }
