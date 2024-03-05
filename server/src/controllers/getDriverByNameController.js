98b971368d84c4739bb9d48b75aa7f05f7d9318e
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-03-05 10:09:34 -0300

    getByName Controller Flexible Search

--- server/src/controllers/getDriverByNameController.js
+++ server/src/controllers/getDriverByNameController.js
@@ -1,4 +1,50 @@
-const getAllDrivers = require ('../controllers/getAllDriversController');
+const getAllDrivers = require('../controllers/getAllDriversController');
+
+// Función para normalizar el texto eliminando acentos, guiones y espacios
+const normalizeText = (text) => {
+    return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/-/g, "").replace(/\s+/g, "").toLowerCase();
+};
+
+// Función para crear una expresión regular para búsqueda flexible
+const createSearchRegex = (searchName) => {
+    const normalizedSearchName = normalizeText(searchName);
+    const regexString = '^' + normalizedSearchName.split('').join('.*');
+    return new RegExp(regexString);
+};
+
+const getDriverByName = async (searchName) => {
+    const allDrivers = await getAllDrivers();
+
+    // Crear expresión regular para búsqueda flexible
+    const searchRegex = createSearchRegex(searchName);
+
+    // Filtrar los conductores por nombre usando la expresión regular
+    const exactMatches = [];
+    const partialMatches = [];
+    for (const driver of allDrivers) {
+        const normalizedForename = normalizeText(driver.forename);
+        if (normalizedForename === normalizeText(searchName)) {
+            exactMatches.push(driver);
+        } else if (normalizedForename.includes(normalizeText(searchName))) {
+            partialMatches.push(driver);
+        }
+    }
+
+    // Concatenar las coincidencias exactas y parciales, y ordenarlas alfabéticamente
+    const sortedDrivers = exactMatches.concat(partialMatches).sort((a, b) => {
+        return normalizeText(a.forename).localeCompare(normalizeText(b.forename));
+    });
+
+    return sortedDrivers.slice(0, 15);
+};
+
+module.exports = getDriverByName;
+
+
+
+
+
+/*const getAllDrivers = require ('../controllers/getAllDriversController');
 
 
 const getDriverByName = async(forename) => {
@@ -10,3 +56,4 @@ const getDriverByName = async(forename) => {
 
 module.exports = getDriverByName;
 
+*/
