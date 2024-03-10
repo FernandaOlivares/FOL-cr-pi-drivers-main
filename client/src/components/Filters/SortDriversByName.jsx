992d52c86ec5ce1e5e8c290aae4cfb23ca4b2aa9
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-03-10 15:26:55 -0300

    DortByName name updated

--- client/src/components/Filters/SortDriversByName.jsx
+++ client/src/components/Filters/SortDriversByName.jsx
@@ -8,7 +8,7 @@ const SortDriversByName = ({handleSortByName, selectedByNameValue}) => {
 return (
   <div className={styles.filterContainer}>
     <select value={selectedByNameValue} onChange={handleSortByName} name='sortByName' id='sortByName' className={styles.filterBtn}>
-        <option value='' disabled id='sortByName'>Full Name</option>
+        <option value='' disabled id='sortByName'>Forename</option>
         <option value='FromAToZ'>From A to Z</option>
         <option value='FromZToA'>From Z to A</option>
     </select>
