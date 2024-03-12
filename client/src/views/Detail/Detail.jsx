28bc0e68f7299671427ff9d5ee5bc9647dd6f1f9
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-03-12 17:09:15 -0300

    Detail Updated

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom'; 
